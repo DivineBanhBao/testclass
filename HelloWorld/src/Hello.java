ebd1a45db5d460229a9cac5094233e6cf293dec7
DivinieBanhBao <isaacnguyen123@gmail.com>
2022-02-25 13:54:19 -0600

    Paul was added

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		System.out.println("Hello my name is Isaac!");
 		System.out.println("Hi my name is Jack");
 		System.out.println("Hi I'm Steve");
+		System.out.println("Hi There I'm Paul");
 	}
 
 }
