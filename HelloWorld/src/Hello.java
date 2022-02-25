e8fddd77db9515c6e408a10266e6070ee7e518ee
DivinieBanhBao <isaacnguyen123@gmail.com>
2022-02-25 14:12:08 -0600

    Revert "Greg was added"
    This reverts commit fe9c8c67fbc740da9ca9103895f40694be984093.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -6,7 +6,6 @@ public class Hello {
 		System.out.println("Hi my name is Jack");
 		System.out.println("Hi I'm Steve");
 		System.out.println("Hi There I'm Raul");
-		System.out.println("What up the name is Greg");
 		
 	}
 
